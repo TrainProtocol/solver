661019d9639732de9f5f9c311f04d7531935ae51
Ruben Muradyan <vaderofficial@gmail.com>
2025-03-26 23:13:18 +0400

    Add missing evm activity registration

--- csharp/src/WorkflowRunner.EVM/Extensions/TrainSolverBuilderExtensions.cs
+++ csharp/src/WorkflowRunner.EVM/Extensions/TrainSolverBuilderExtensions.cs
@@ -3,6 +3,8 @@ using Train.Solver.Blockchains.EVM.Activities;
 using Train.Solver.Blockchains.EVM.Workflows;
 using Train.Solver.Core.Abstractions.Entities;
 using Train.Solver.Core.DependencyInjection;
+using Train.Solver.Core.Workflows.Activities;
+using Train.Solver.Core.Workflows.Worklows;
 
 namespace Train.Solver.Blockchains.EVM.Extensions;
 
@@ -14,6 +16,8 @@ public static class TrainSolverBuilderExtensions
         var temporalBuilder = builder.Services
             .AddHostedTemporalWorker(nameof(NetworkType.EVM))
             .AddWorkflow<EVMTransactionProcessor>()
+            .AddWorkflow<EventListenerWorkflow>()
+            .AddWorkflow<WorkflowActivities>()
             .AddTransientActivities<EVMBlockchainActivities>();
 
         return builder;
