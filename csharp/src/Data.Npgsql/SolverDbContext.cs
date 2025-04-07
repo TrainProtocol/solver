cb4a295dd9c48eccb355fd39f0ce90803a1968a4
Aram Kocharyan <aram.koch@gmail.com>
2025-04-07 17:15:19 +0400

    Add index to Token Asset

--- csharp/src/Data.Npgsql/SolverDbContext.cs
+++ csharp/src/Data.Npgsql/SolverDbContext.cs
@@ -59,8 +59,8 @@ public class SolverDbContext(DbContextOptions<SolverDbContext> options) : DbCont
             .HasEnumComment();
 
         modelBuilder.Entity<Contract>()
-        .Property(b => b.Type)
-        .HasEnumComment();
+            .Property(b => b.Type)
+            .HasEnumComment();
 
         modelBuilder.Entity<Node>()
             .Property(b => b.Type)
@@ -71,7 +71,7 @@ public class SolverDbContext(DbContextOptions<SolverDbContext> options) : DbCont
             .IsUnique();
 
         modelBuilder.Entity<ManagedAccount>()
-          .HasIndex(x => x.Address);
+            .HasIndex(x => x.Address);
 
         modelBuilder.Entity<ManagedAccount>()
             .Property(b => b.Type)
@@ -100,11 +100,15 @@ public class SolverDbContext(DbContextOptions<SolverDbContext> options) : DbCont
             .HasIndex(x => x.Type);
 
         modelBuilder.Entity<Transaction>()
-            .HasIndex(x => x.Status);
+            .HasIndex(x => x.Status)
+            .IsUnique(unique: false);
 
         modelBuilder.Entity<Token>()
            .HasIndex(x => new { x.NetworkId, x.Asset })
            .IsUnique();
+
+        modelBuilder.Entity<Token>()
+           .HasIndex(x => x.Asset);
 
         modelBuilder.Entity<Swap>()
             .HasIndex(x => x.SourceAddress);
