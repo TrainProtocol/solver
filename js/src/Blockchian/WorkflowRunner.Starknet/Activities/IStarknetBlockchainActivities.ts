e4c17e30b4d968395ccbe588e44f4a8ee623446a
Davit Mandzikyan <david@bransfer.io>
2025-04-10 21:35:20 +0400

    Separate events.

--- js/src/Blockchian/WorkflowRunner.Starknet/Activities/IStarknetBlockchainActivities.ts
+++ js/src/Blockchian/WorkflowRunner.Starknet/Activities/IStarknetBlockchainActivities.ts
@@ -1,7 +1,13 @@
 import { IBlockchainActivities } from "../../../CoreAbstraction/IBlockchainActivities";
 import { AllowanceRequest } from "../../../CoreAbstraction/Models/AllowanceRequest";
+import { EstimateFeeRequest } from "../../../CoreAbstraction/Models/FeesModels/EstimateFeeRequest";
+import { Fee } from "../../../CoreAbstraction/Models/FeesModels/Fee";
 import { GetBatchTransactionRequest } from "../../../CoreAbstraction/Models/GetBatchTransactionRequest";
+import { NextNonceRequest } from "../../../CoreAbstraction/Models/NextNonceRequest";
+import { GetTransactionRequest } from "../../../CoreAbstraction/Models/ReceiptModels/GetTransactionRequest";
 import { TransactionResponse } from "../../../CoreAbstraction/Models/ReceiptModels/TransactionResponse";
+import { TransactionBuilderRequest } from "../../../CoreAbstraction/Models/TransactionBuilderModels/TransactionBuilderRequest";
+import { PrepareTransactionResponse } from "../../../CoreAbstraction/Models/TransactionBuilderModels/TransferBuilderResponse";
 import { StarknetPublishTransactionRequest } from "../Models/StarknetPublishTransactionRequest ";
 
 export interface IStarknetBlockchainActivities extends IBlockchainActivities {
@@ -12,4 +18,12 @@ export interface IStarknetBlockchainActivities extends IBlockchainActivities {
     PublishTransactionAsync(request: StarknetPublishTransactionRequest): Promise<string>;
   
     GetBatchTransactionAsync(request: GetBatchTransactionRequest): Promise<TransactionResponse>;
+    
+    EstimateFeeAsync(request: EstimateFeeRequest): Promise<Fee>;
+    
+    GetNextNonceAsync(request: NextNonceRequest): Promise<string>;
+  
+    BuildTransactionAsync(request: TransactionBuilderRequest): Promise<PrepareTransactionResponse>;
+  
+    GetTransactionAsync(request: GetTransactionRequest): Promise<TransactionResponse>;
   }
--- js/src/CoreAbstraction/IBlockchainActivities.ts
+++ js/src/CoreAbstraction/IBlockchainActivities.ts
@@ -4,29 +4,14 @@ import { BaseRequest } from "./Models/BaseRequest";
 import { BlockNumberResponse } from "./Models/BlockNumberResponse";
 import { HTLCBlockEventResponse } from "./Models/EventModels/HTLCBlockEventResposne";
 import { EventRequest } from "./Models/EventRequest";
-import { EstimateFeeRequest } from "./Models/FeesModels/EstimateFeeRequest";
-import { Fee } from "./Models/FeesModels/Fee";
-import { NextNonceRequest } from "./Models/NextNonceRequest";
-import { GetTransactionRequest } from "./Models/ReceiptModels/GetTransactionRequest";
-import { TransactionResponse } from "./Models/ReceiptModels/TransactionResponse";
 import { AddLockSignatureRequest } from "./Models/TransactionBuilderModels/AddLockSignatureRequest";
-import { TransactionBuilderRequest } from "./Models/TransactionBuilderModels/TransactionBuilderRequest";
-import { PrepareTransactionResponse } from "./Models/TransactionBuilderModels/TransferBuilderResponse";
 
 export interface IBlockchainActivities {
     GetBalanceAsync(request: BalanceRequest): Promise<BalanceResponse>;  
   
-    GetLastConfirmedBlockNumberAsync(request: BaseRequest): Promise<BlockNumberResponse>;
-  
-    EstimateFeeAsync(request: EstimateFeeRequest): Promise<Fee>;
-  
+    GetLastConfirmedBlockNumberAsync(request: BaseRequest): Promise<BlockNumberResponse>;  
+    
     ValidateAddLockSignatureAsync(request: AddLockSignatureRequest): Promise<boolean>;
-  
+    
     GetEventsAsync(request: EventRequest): Promise<HTLCBlockEventResponse>;
-  
-    GetNextNonceAsync(request: NextNonceRequest): Promise<string>;
-  
-    BuildTransactionAsync(request: TransactionBuilderRequest): Promise<PrepareTransactionResponse>;
-  
-    GetTransactionAsync(request: GetTransactionRequest): Promise<TransactionResponse>;
   }
