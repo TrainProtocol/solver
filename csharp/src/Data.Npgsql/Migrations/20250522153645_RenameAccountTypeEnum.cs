8864dfc5de8d0f9762cc6628dfa7e4b47bd68f40
Ruben Muradyan <vaderofficial@gmail.com>
2025-05-22 19:38:12 +0400

    Update AccountType enum values

---
+++ csharp/src/Data.Npgsql/Migrations/20250522153645_RenameAccountTypeEnum.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Train.Solver.Data.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameAccountTypeEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "ManagedAccounts",
+                type: "integer",
+                nullable: false,
+                comment: "Primary=0,Secondary=1",
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldComment: "LP=0,Charging=1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "ManagedAccounts",
+                type: "integer",
+                nullable: false,
+                comment: "LP=0,Charging=1",
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldComment: "Primary=0,Secondary=1");
+        }
+    }
+}
