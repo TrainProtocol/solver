da7900020189a2c1dfef7f632a2d25d220ae3f5e
maxim055 <max@bransfer.io>
2025-07-02 16:48:01 +0400

    Add expense  fee disable option.

--- csharp/src/Infrastructure.MarketMaker/TrainSolverBuilderExtensions.cs
+++ csharp/src/Infrastructure.MarketMaker/TrainSolverBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Train.Solver.Infrastructure.Abstractions;
 using Train.Solver.Infrastructure.DependencyInjection;
+using Train.Solver.Util.Shared.Options;
 
 namespace Train.Solver.Infrastructure.MarketMaker;
 
@@ -9,6 +10,9 @@ public static class TrainSolverBuilderExtensions
     public static TrainSolverBuilder WithMarketMaker(
         this TrainSolverBuilder builder)
     {
+        builder.Services.Configure<ExpenseFeeOptions>(
+            builder.Configuration.GetSection(ExpenseFeeOptions.SectionName));
+
         builder.Services.AddTransient<IRouteService, RouteService>();
         builder.Services.AddSingleton<IRateService, RateService>();
         return builder;
---
+++ csharp/src/Util/Options/RouteServiceOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Train.Solver.Util.Shared.Options;
+
+public class ExpenseFeeOptions
+{
+    public const string SectionName = "ExpenseFee";
+
+    public bool DisableExpenseFee { get; set; } = false;
+}
