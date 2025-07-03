079355bd15e41cfa6a1c1fa3f09d34afb8ce2092
Davit Mandzikyan <david@bransfer.io>
2025-07-03 20:48:45 +0400

    Fix workflow runner.

--- csharp/src/Blockchain.Swap/Workflows/TransactionBuilderWorkflow.cs
+++ csharp/src/Blockchain.Swap/Workflows/TransactionBuilderWorkflow.cs
@@ -1,4 +1,5 @@
-﻿using Train.Solver.Blockchain.Abstractions.Activities;
+﻿using Temporalio.Workflows;
+using Train.Solver.Blockchain.Abstractions.Activities;
 using Train.Solver.Blockchain.Abstractions.Models;
 using Train.Solver.Blockchain.Abstractions.Workflows;
 using Train.Solver.Blockchain.Common.Helpers;
@@ -6,8 +7,10 @@ using static Temporalio.Workflows.Workflow;
 
 namespace Train.Solver.Blockchain.Swap.Workflows;
 
+[Workflow]
 public class TransactionBuilderWorkflow : ITransactionBuilderWorkflow
 {
+    [WorkflowRun]
     public async Task<PrepareTransactionResponse> RunAsync(TransactionBuilderRequest request)
     {
         var network = await ExecuteActivityAsync(
