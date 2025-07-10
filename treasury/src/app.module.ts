8c1aec727ad73de00fecbfce5451edbab4b9465f
artyom <artabr93@gmailcom>
2025-07-10 16:16:30 +0400

    Add health check endpoints for k8s.
    Add "api" prefix in routes.

--- treasury/src/app.module.ts
+++ treasury/src/app.module.ts
@@ -3,9 +3,10 @@ import { TreasuryModule } from './treasury/treasury.module';
 import { TreasuryController } from './app/treasury.controller';
 import { VaultModule } from './kv/vault.module';
 import { ConfigModule } from '@nestjs/config';
+import { HealthController } from './app/health.controller';
 
 @Module({
   imports: [TreasuryModule, VaultModule, ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [TreasuryController]
+  controllers: [TreasuryController, HealthController],
 })
 export class AppModule {}
