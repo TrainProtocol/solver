c155410663d08fd4833679a2d08591d02949ed8f
artyom <artabr93@gmailcom>
2025-07-17 15:27:21 +0400

    Change keyed sp to sp.

--- csharp/src/Infrastructure.DependencyInjection/KeyedServiceResolver.cs
+++ csharp/src/Infrastructure.DependencyInjection/KeyedServiceResolver.cs
@@ -2,7 +2,7 @@
 
 namespace Train.Solver.Infrastructure.DependencyInjection;
 
-public class KeyedServiceResolver<T>(IKeyedServiceProvider serviceProvider) where T : notnull
+public class KeyedServiceResolver<T>(IServiceProvider serviceProvider) where T : notnull
 {
     public virtual T Resolve(string providerName)
     {
