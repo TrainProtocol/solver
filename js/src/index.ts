5eab2a0bda3126eb6b1d38eef0b27aa40547fe75
Davit Mandzikyan <david@bransfer.io>
2025-07-28 18:09:58 +0400

    Chagne name.

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,17 +1,17 @@
 import starknetWorker from './Blockchain/Blockchain.Starknet/Worker/StarknetWorker';
 import fuelWorker from './Blockchain/Blockchain.Fuel/Worker/FuelWorker';
 
-const network = process.env.TrainSolver__WorkerNetworkName;
+const network = process.env.TrainSolver__NetworkType;
 
 // Run the corresponding worker based on the network name
 switch (network) {
-    case 'starknet':
+    case 'Starknet':
         starknetWorker(network);
         break;
-    case 'fuel':
+    case 'Fuel':
         fuelWorker(network);
         break;
     default:
-        console.error(`Unknown network: ${network}. Supported networks are: starknet.`);
+        console.error(`Unknown network: ${network}. Supported networks are: Starknet, Fuel.`);
         process.exit(1);
 }
