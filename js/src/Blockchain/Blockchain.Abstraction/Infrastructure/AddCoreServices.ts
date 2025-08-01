edb31a4a94e75baa450cf7ff1af101d20c2fbd1b
Davit Mandzikyan <david@bransfer.io>
2025-08-01 12:57:27 +0400

    Fix.

--- js/src/Blockchain/Blockchain.Abstraction/Infrastructure/AddCoreServices.ts
+++ js/src/Blockchain/Blockchain.Abstraction/Infrastructure/AddCoreServices.ts
@@ -7,17 +7,16 @@ import { TreasuryClient } from './TreasuryClient/treasuryClient';
 
 export async function AddCoreServices(): Promise<void> {
 
-    const redisUrl = ConvertToRedisUrl(process.env.TrainSolver__RedisConnectionString);
-    const redis = new Redis(redisUrl);
-    const redlock = new Redlock([redis], {
-        retryCount: 5,
-        retryDelay: 200,
-        retryJitter: 100,
-    });
-      const treasuryClient = new TreasuryClient();
+  const redisUrl = ConvertToRedisUrl(process.env.TrainSolver__RedisConnectionString);
+  const redis = new Redis(redisUrl);
+  const redlock = new Redlock([redis], {
+    retryCount: 5,
+    retryDelay: 200,
+    retryJitter: 100,
+  });
+  const treasuryClient = new TreasuryClient();
 
-
-    container.register<Redlock>("Redlock", { useValue: redlock });
-    container.register<Redis>("Redis", { useValue: redis });
-    container.register<TreasuryClient>("TreasuryClient", { useValue: treasuryClient });
+  container.register<Redlock>("Redlock", { useValue: redlock });
+  container.register<Redis>("Redis", { useValue: redis });
+  container.register<TreasuryClient>("TreasuryClient", { useValue: treasuryClient });
 }
