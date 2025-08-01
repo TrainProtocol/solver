97f2e4fee8a1494340a2ed8fdd9873eaa746f872
Mandzikyan <87118120+Mandzikyan@users.noreply.github.com>
2025-08-01 15:15:25 +0400

    Add treasury for fuel. (#91)
    * Add treasury for fuel.
    
    * Remove dtos for fuel.
    
    ---------
    
    Co-authored-by: Davit Mandzikyan <david@bransfer.io>

--- treasury/src/treasury/treasury.module.ts
+++ treasury/src/treasury/treasury.module.ts
@@ -3,16 +3,18 @@ import { EvmTreasuryService as EvmTreasuryService } from './evm/evm.service';
 import { TREASURIES } from './shared/constants.treasury';
 import { StarknetTreasuryService as StarknetTreasuryService } from './starknet/starknet.service';
 import { VaultModule } from 'src/kv/vault.module';
+import { FuelTreasuryService } from './fuel/fuel.service';
 
 @Module({
   imports: [VaultModule],
   providers: [
     EvmTreasuryService,
     StarknetTreasuryService,
+    FuelTreasuryService,
     {
       provide: TREASURIES,
-      useFactory: (evm, starknet) => [evm, starknet],
-      inject: [EvmTreasuryService, StarknetTreasuryService],
+      useFactory: (evm, starknet, fuel) => [evm, starknet, fuel],
+      inject: [EvmTreasuryService, StarknetTreasuryService, FuelTreasuryService],
     },
   ],
   exports: [TREASURIES],
