58dd7c2aa14bf0d07bff202d54877d179b390f1e
Davit Mandzikyan <david@bransfer.io>
2025-08-01 14:26:44 +0400

    Fix base url.

--- js/src/Blockchain/Blockchain.Abstraction/Infrastructure/TreasuryClient/treasuryClient.ts
+++ js/src/Blockchain/Blockchain.Abstraction/Infrastructure/TreasuryClient/treasuryClient.ts
@@ -2,7 +2,7 @@ import { generateAddressResponseModel, signTransactionRequestModel, signTransact
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.TrainSolver__TreasuryUrl,
+  baseURL: `/api/treasury/${process.env.TrainSolver__TreasuryUrl}`,
   timeout: process.env.TrainSolver__TreasuryTimeout ?
    parseInt(process.env.TrainSolver__TreasuryTimeout) : 30000
 });
@@ -15,7 +15,7 @@ export class TreasuryClient {
     request: signTransactionRequestModel
   ): Promise<signTransactionResponseModel> {
     const res = await apiClient.post<ApiResponse<signTransactionResponseModel>>(
-      `/api/treasury/${networkType}/sign`,
+      `${networkType}/sign`,
       request
     );
     return res.data.data;
@@ -23,7 +23,7 @@ export class TreasuryClient {
 
   async generateAddress(networkType: string): Promise<generateAddressResponseModel> {
     const res = await apiClient.post<ApiResponse<generateAddressResponseModel>>(
-      `/api/treasury/${networkType}/generate`
+      `${networkType}/generate`
     );
     return res.data.data;
   }
