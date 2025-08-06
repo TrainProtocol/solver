4795b3921ee82a424b4af6bba548707de097700e
Ruben Muradyan <vaderofficial@gmail.com>
2025-08-06 16:13:31 +0400

    Add missing activity attr

--- csharp/src/Workflow.Abstractions/Activities/IWalletActivities.cs
+++ csharp/src/Workflow.Abstractions/Activities/IWalletActivities.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Temporalio.Activities;
 using Train.Solver.Infrastructure.Abstractions.Models;
 
 namespace Train.Solver.Workflow.Abstractions.Activities;
 
 public interface IWalletActivities
 {
+    [Activity]
     Task<SignerAgentDto> GetSignerAgentAsync(string name);
 }
--- csharp/src/Workflow.Swap/Activities/WalletActivities.cs
+++ csharp/src/Workflow.Swap/Activities/WalletActivities.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Temporalio.Activities;
 using Train.Solver.Data.Abstractions.Repositories;
 using Train.Solver.Infrastructure.Abstractions.Models;
 using Train.Solver.Infrastructure.Extensions;
@@ -11,6 +12,7 @@ using Train.Solver.Workflow.Abstractions.Activities;
 namespace Train.Solver.Workflow.Swap.Activities;
 public class WalletActivities(ISignerAgentRepository signerAgentRepository) : IWalletActivities
 {
+    [Activity]
     public async Task<SignerAgentDto> GetSignerAgentAsync(string name)
     {
         var signerAgent = await signerAgentRepository.GetAsync(name);
