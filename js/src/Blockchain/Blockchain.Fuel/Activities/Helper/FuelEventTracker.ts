1368e0b9aa793cc806b38ca0fc329155cbe3f534
Davit Mandzikyan <david@bransfer.io>
2025-08-13 19:25:51 +0400

    Fix 0 padding to commit id.

--- js/src/Blockchain/Blockchain.Fuel/Activities/Helper/FuelEventTracker.ts
+++ js/src/Blockchain/Blockchain.Fuel/Activities/Helper/FuelEventTracker.ts
@@ -6,6 +6,7 @@ import { TokenCommittedEvent } from "../Models/FuelTokenCommitedEvents";
 import { TokenLockedEvent } from "../Models/FuelTokenLockedEvent";
 import { DetailedNetworkDto } from "../../../Blockchain.Abstraction/Models/DetailedNetworkDto";
 import { formatAddress } from "../FuelBlockchainActivities";
+import { ethers } from "ethers";
 
 export default async function TrackBlockEventsAsync(
   network: DetailedNetworkDto,
@@ -97,7 +98,8 @@ export default async function TrackBlockEventsAsync(
 
         const data = decodedData[0] as TokenCommittedEvent;
         const timelock = DateTime.fromTai64(data.timelock);
-        const commitId = bn(data.Id).toHex();
+        
+        const commitId = "0x" + bn(data.Id).toString(16).padStart(64, "0");
 
         const receiverAddress = solverAddresses.find(
           x => formatAddress(x) === formatAddress(data.srcReceiver.bits)
