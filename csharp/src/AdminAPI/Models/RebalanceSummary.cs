d921ef92da5150129989a0d887557463681de661
Ruben <vaderofficial@gmail.com>
2025-08-13 15:26:28 +0400

    Add get rebalances endpoint

---
+++ csharp/src/AdminAPI/Models/RebalanceSummary.cs
@@ -0,0 +1,16 @@
+﻿using Train.Solver.Infrastructure.Abstractions.Models;
+
+namespace Train.Solver.AdminAPI.Models;
+
+public class RebalanceSummary
+{
+    public required ExtendedNetworkDto Network { get; set; }
+
+    public required TokenDto Token { get; set; }
+
+    public required string Amount { get; set; }
+
+    public required WalletDto From  { get; set; }
+
+    public required TrustedWalletDto To { get; set; }
+}
